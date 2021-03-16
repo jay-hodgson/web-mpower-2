c77616c27a29e9f91c03be7af9428cd2e8471902
Jay Hodgson <jay.hodgson@sagebase.org>
2021-03-16 13:58:49 -0700

    more progress on consent

--- src/components/study/Consent.vue
+++ src/components/study/Consent.vue
@@ -16,7 +16,7 @@
           </div>
           <p>{{ $t('consent-screen.step-1.install-on-phone') }}</p>
 
-          <p>{{ $t('consent-screen.step-1.use-app-to') }}:</p>
+          <p>{{ $t('consent-screen.step-1.use-app-to') }}</p>
 
           <ul>
             <li>{{ $t('consent-screen.step-1.answer-health-questions') }} </li>
@@ -54,83 +54,83 @@
         <div v-show="step === 4">
           <div class="header">
             <object data="/static/images/future%20research.svg" type="image/svg+xml"></object>
-            <h3>Sharing for Future Research</h3>
+            <h3>{{ $t('consent-screen.step-4.title') }}</h3>
           </div>
           <ul>
-            <li>Sharing your coded study data broadly (without information such as your name) may benefit this and future research.</li>
-            <li>You get to decide if you want to share your coded study data broadly with qualified researchers.</li>
-            <li>Once we have shared your data with other researchers we cannot withdraw it.</li>
+            <li>{{ $t('consent-screen.step-4.sharing-data') }}</li>
+            <li>{{ $t('consent-screen.step-4.share-with-researchers') }}</li>
+            <li>{{ $t('consent-screen.step-4.cannot-withdraw') }}</li>
           </ul>
         </div>
         <div v-show="step === 5">
           <div class="header">
             <object data="/static/images/Risks.svg" type="image/svg+xml"></object>
-            <h3>Potential Risks</h3>
+            <h3>{{ $t('consent-screen.step-5.title') }}</h3>
           </div>
           <ul>
-            <li>We expect the risk of participating in this study to be low.</li>
-            <li>Some of the activities and questions we ask could be tiring, frustrating, boring or may make you feel uncomfortable.</li>
-            <li>Seeing your data may cause many emotions. </li>
-            <li>We will do our best to protect your information, but your data could be hacked.</li>
-            <li>There may be other risks to participating that we do not know about yet.</li>
+            <li>{{ $t('consent-screen.step-5.low-participation') }}</li>
+            <li>{{ $t('consent-screen.step-5.tiring') }}</li>
+            <li>{{ $t('consent-screen.step-5.emotions') }}</li>
+            <li>{{ $t('consent-screen.step-5.hacked') }}</li>
+            <li>{{ $t('consent-screen.step-5.unknowns') }}</li>
           </ul>
         </div>
         <div v-show="step === 6">
           <div class="header">
             <object data="/static/images/Benefits.svg" type="image/svg+xml"></object>
-            <h3>Potential Benefits</h3>
+            <h3>{{ $t('consent-screen.step-6.title') }}</h3>
           </div>
           <ul>
-            <li>You may not directly benefit from taking part but seeing trends in your data may be interesting to you. </li>
-            <li>You can share this data with your doctors, family members, or others.</li>
-            <li>You may help researchers better understand Parkinson’s Disease. This may help future generations. </li>
+            <li>{{ $t('consent-screen.step-6.interesting-trends') }} </li>
+            <li>{{ $t('consent-screen.step-6.share') }}</li>
+            <li>{{ $t('consent-screen.step-6.help-all') }}</li>
           </ul>
         </div>
         <div v-show="step === 7">
           <div class="header">
             <object data="/static/images/Not%20medical%20care.svg" type="image/svg+xml"></object>
-            <h3>NOT medical care</h3>
+            <h3>{{ $t('consent-screen.step-7.title') }}</h3>
           </div>
           <ul>
-            <li>mPower is a research study. </li>
-            <li>mPower does not provide medical care, medical advice, or treatment.</li>
-            <li>The mPower app is not a diagnosis tool. </li>
-            <li>Contact your health provider if you have questions or concerns related to your health, or if you need medical care.</li>
+            <li>{{ $t('consent-screen.step-7.research-study') }} </li>
+            <li>{{ $t('consent-screen.step-7.not-medical-care') }}</li>
+            <li>{{ $t('consent-screen.step-7.not-diagnosis-tool') }} </li>
+            <li>{{ $t('consent-screen.step-7.concerns') }}</li>
           </ul>
         </div>
         <div v-show="step === 8">
           <div class="header">
             <object data="/static/images/Issues%20to%20consider.svg" type="image/svg+xml"></object>
-            <h3>Issues to Consider</h3>
+            <h3>{{ $t('consent-screen.step-8.title') }}</h3>
           </div>
           <ul>
-            <li>The time you use on this app may count against your mobile data plan. You can set up the app to use Wi-Fi connections instead. </li>
-            <li>You will not be paid for participating in this study. You will not be paid for sharing your data. </li>
-            <li>You will not receive any profit from this or future research. </li>
-            <li>The risk of injury is low in this study. You will not be compensated for injury.</li>
+            <li>{{ $t('consent-screen.step-8.data-plan') }}</li>
+            <li>{{ $t('consent-screen.step-8.not-paid') }}</li>
+            <li>{{ $t('consent-screen.step-8.no-profit') }} </li>
+            <li>{{ $t('consent-screen.step-8.injury') }}</li>
           </ul>
         </div>
         <div v-show="step === 9">
           <div class="header">
             <object data="/static/images/Leave.svg" type="image/svg+xml"></object>
-            <h3>Leaving the Study</h3>
+            <h3>{{ $t('consent-screen.step-9.title') }}</h3>
           </div>
           <ul>
-            <li>Your participation is voluntary.</li>
-            <li>You may leave the study at any time and for any reason.</li>
-            <li>If you leave the study, you withdraw your consent.</li>
-            <li>Your study data from before your withdrawal will continue to be used in the study. It will not be destroyed or deleted.</li>
+            <li>{{ $t('consent-screen.step-9.voluntary') }}</li>
+            <li>{{ $t('consent-screen.step-9.leave') }}</li>
+            <li>{{ $t('consent-screen.step-9.withdraw-consent') }}</li>
+            <li>{{ $t('consent-screen.step-9.before-withdraw') }}</li>
           </ul>
         </div>
         <div v-show="step === 10">
           <div class="header">
             <object data="/static/images/contact.svg" type="image/svg+xml"></object>
-            <h3>Contact</h3>
+            <h3>{{ $t('consent-screen.step-10.title') }}</h3>
           </div>
           <ul>
-            <li>If you have questions you can reach us by email at PDApp@sagebionetworks.org or call toll free +1.833-SAGEBIO.</li>
-            <li>We may want to reach out to you.</li>
-            <li>You can opt out of these follow up notifications at any time</li>
+            <li>{{ $t('consent-screen.step-10.email') }}</li>
+            <li>{{ $t('consent-screen.step-10.reach-out') }}</li>
+            <li>{{ $t('consent-screen.step-10.opt-out') }}</li>
           </ul>
         </div>
         <p class="more"><a href="#" @click.prevent="showMore">See this on the information sheet.</a></p>
